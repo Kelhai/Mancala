931e846b835de58235f63a856134545ba055cae9
HPro2 <hpro2@SV156-12-039014.sylvaniaschools.local>
2022-05-17 10:29:12 -0400

    Initial commit

---
+++ app/src/main/java/com/jonesclass/mancala/MainActivity.java
@@ -0,0 +1,27 @@
+package com.jonesclass.mancala;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), Game.class);
+                startActivity(startIntent);
+            }
+        });
+    }
+}
